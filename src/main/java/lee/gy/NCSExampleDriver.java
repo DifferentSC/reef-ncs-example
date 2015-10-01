0a627d193aa9cca31504184d186dd6637e9bef31
DifferentSC <bert1234@snu.ac.kr>
2015-10-01 22:32:43 +0900

    Implement example driver for NCS

--- src/main/java/lee/gy/NCSExampleDriver.java
+++ src/main/java/lee/gy/NCSExampleDriver.java
@@ -1,19 +1,26 @@
 package lee.gy;
 
 import com.google.inject.Inject;
+import org.apache.reef.driver.context.ActiveContext;
 import org.apache.reef.driver.context.ContextConfiguration;
 import org.apache.reef.driver.evaluator.AllocatedEvaluator;
 import org.apache.reef.driver.evaluator.EvaluatorRequest;
 import org.apache.reef.driver.evaluator.EvaluatorRequestor;
+import org.apache.reef.driver.task.RunningTask;
+import org.apache.reef.driver.task.TaskConfiguration;
+import org.apache.reef.io.network.naming.NameResolverConfiguration;
 import org.apache.reef.io.network.naming.NameServer;
-import org.apache.reef.tang.Injector;
-import org.apache.reef.tang.Tang;
+import org.apache.reef.io.network.util.StringIdentifierFactory;
+import org.apache.reef.tang.*;
 import org.apache.reef.tang.annotations.Unit;
 import org.apache.reef.tang.exceptions.InjectionException;
 import org.apache.reef.wake.EventHandler;
+import org.apache.reef.wake.IdentifierFactory;
 import org.apache.reef.wake.time.event.StartTime;
-import org.reflections.Configuration;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,14 +29,29 @@ public class NCSExampleDriver {
 
   private static final Logger LOG = Logger.getLogger(NCSExampleDriver.class.getName());
   private final EvaluatorRequestor requestor;
-  private NameServer nameServer;
+  private final NameServer nameServer;
+  private final String senderName, receiverName;
+  private final AtomicInteger submittedTask;
+  private ActiveContext senderContext;
+  private String driverHostAddress;
 
   @Inject
   NCSExampleDriver(final EvaluatorRequestor requestor) throws InjectionException {
     this.requestor = requestor;
     LOG.log(Level.FINE, "NCSExampleDriver started");
     Injector injector = Tang.Factory.getTang().newInjector();
-    nameServer = injector.getInstance(NameServer.class);
+    this.nameServer = injector.getInstance(NameServer.class);
+    this.submittedTask = new AtomicInteger();
+    this.submittedTask.set(0);
+    this.senderName = "sender";
+    this.receiverName = "receiver";
+
+    try {
+      this.driverHostAddress = Inet4Address.getLocalHost().getHostAddress();
+    } catch(UnknownHostException e) {
+      LOG.log(Level.SEVERE, "Cannot get driver host address! " + e.toString());
+      this.driverHostAddress = "127.0.0.1";
+    }
   }
 
   public final class StartHandler implements EventHandler<StartTime> {
@@ -48,7 +70,53 @@ public class NCSExampleDriver {
     @Override
     public void onNext(final AllocatedEvaluator evaluator) {
       LOG.log(Level.FINE, "Evaluator allocated");
-      final Configuration contextConf = ContextConfiguration.
+      Configuration contextConf = ContextConfiguration.CONF.build();
+      evaluator.submitContext(contextConf);
+    }
+  }
+
+  public final class ActiveContextHandler implements EventHandler<ActiveContext> {
+    @Override
+    public void onNext(final ActiveContext context) {
+      if (submittedTask.compareAndSet(0, 1)) {
+        // keep context for the sender task
+        senderContext = context;
+      } else {
+        Configuration partialTaskConf = TaskConfiguration.CONF
+            .set(TaskConfiguration.IDENTIFIER, "receiver_task")
+            .build();
+        Configuration netConf = NameResolverConfiguration.CONF
+            .set(NameResolverConfiguration.NAME_SERVER_HOSTNAME, driverHostAddress)
+            .set(NameResolverConfiguration.NAME_SERVICE_PORT, NCSExampleDriver.this.nameServer.getPort())
+            .build();
+        JavaConfigurationBuilder taskConfBuilder =
+            Tang.Factory.getTang().newConfigurationBuilder(partialTaskConf, netConf);
+        taskConfBuilder.bindNamedParameter(NCSReceiverTask.ReceiverName.class, receiverName);
+        Configuration taskConf = taskConfBuilder.build();
+        context.submitTask(taskConf);
+      }
+    }
+  }
+
+  public final class RunningTaskHandler implements EventHandler<RunningTask> {
+    @Override
+    public void onNext(RunningTask task) {
+      if (task.getId().equals("receiver_task")) {
+        // receiver task is ready, submit sender task
+        Configuration partialTaskConf = TaskConfiguration.CONF
+            .set(TaskConfiguration.IDENTIFIER, "sender_task")
+            .build();
+        Configuration netConf = NameResolverConfiguration.CONF
+            .set(NameResolverConfiguration.NAME_SERVER_HOSTNAME, driverHostAddress)
+            .set(NameResolverConfiguration.NAME_SERVICE_PORT, NCSExampleDriver.this.nameServer.getPort())
+            .build();
+        JavaConfigurationBuilder taskConfBuilder =
+            Tang.Factory.getTang().newConfigurationBuilder(partialTaskConf, netConf);
+        taskConfBuilder.bindNamedParameter(NCSSenderTask.ReceiverName.class, receiverName);
+        taskConfBuilder.bindNamedParameter(NCSSenderTask.SenderName.class, senderName);
+        Configuration taskConf = taskConfBuilder.build();
+        senderContext.submitTask(taskConf);
+      }
     }
   }
 
